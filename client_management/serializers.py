0371137b62f7662c3e24a14f8458085a88ce2150
Yashkank <yashkank1999@gmail.com>
2024-10-09 00:56:33 +0530

    first commit

---
+++ client_management/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Client, Project
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = ['id', 'client_name', 'created_at', 'created_by']
+
+class ProjectSerializer(serializers.ModelSerializer):
+    created_by = serializers.ReadOnlyField(source='created_by.username') 
+
+    class Meta:
+        model = Project
+        fields = ['id', 'project_name', 'client', 'users', 'created_at', 'created_by']
+    
